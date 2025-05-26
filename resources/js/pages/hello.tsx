5e0b0ea8f946770cbd68b9182e3841dac37b201f
Esteban Arancibia <secume01@hperal.local>
2025-05-26 15:49:33 -0400

    initial commit

---
+++ resources/js/pages/hello.tsx
@@ -0,0 +1,11 @@
+
+import { Head } from "@inertiajs/react";
+
+export default function Hello() {
+  return (
+    <>
+      <Head title="Hello World" />
+      <h1>Hola Mundo</h1>
+    </>
+  );
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::get('/hello', function () {
+    return Inertia::render('hello');})->name('hello');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
